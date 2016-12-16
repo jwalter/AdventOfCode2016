38cbd084d1879fcc29cbeeaaca9a46aca3f6e631
jwalter <joakim.waltersson@gmail.com>
2016-12-16 13:08:07 +0100

    Initial commit

---
+++ AdventOfCode.Library.Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+namespace AdventOfCode.Library.Tests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void North()
+		{
+			Direction newDirection = Day01.GetNewDirection(Direction.North);
+			Assert.AreSame(Direction.North, newDirection);
+		}
+	}
+}
---
+++ AdventOfCode.Library/Day01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace AdventOfCode.Library
+{
+	public class Day01
+	{
+		public Day01()
+		{
+		}
+
+		public static Heading GetNewDirection(Heading direction) {
+			Contract.Ensures(Contract.Result<Heading>() != null);
+			return Heading.North;
+		}
+
+	}
+
+	public class Heading
+	{
+		public static Heading North { get; internal set; }
+		public static Heading West { get; internal set; }
+		public static Heading South { get; internal set; }
+		public static Heading East { get; internal set; }
+	}
+}
