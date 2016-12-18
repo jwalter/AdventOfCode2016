1e5cea9b91d6223a4148704c7114228bf8814644
jwalter <joakim.waltersson@gmail.com>
2016-12-18 21:24:32 +0100

    Finish part 1 of Day 1

---
+++ AdventOfCode.Library.Tests/Day01Test.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+namespace AdventOfCode.Library.Tests
+{
+	[TestFixture()]
+	public class Day01Test
+	{
+		[TestCase(Heading.North, Direction.Left, Heading.West)]
+		[TestCase(Heading.North, Direction.Right, Heading.East)]
+		[TestCase(Heading.East, Direction.Left, Heading.North)]
+		[TestCase(Heading.East, Direction.Right, Heading.South)]
+		[TestCase(Heading.South, Direction.Left, Heading.East)]
+		[TestCase(Heading.South, Direction.Right, Heading.West)]
+		[TestCase(Heading.West, Direction.Left, Heading.South)]
+		[TestCase(Heading.West, Direction.Right, Heading.North)]
+		public void GetNewHeading(Heading heading, Direction direction, Heading expected)
+		{
+			Assert.AreEqual(expected, Library.Day01.GetNewHeading(heading, direction));
+		}
+
+		[TestCase('L', Direction.Left)]
+		[TestCase('R', Direction.Right)]
+		public void GetDirection(char input, Direction expected)
+		{
+			Assert.AreEqual(expected, Library.Day01.GetDirection(input));
+		}
+
+		[TestCase("R123", 123)]
+		public void GetDistance(string input, int expected)
+		{
+			Assert.AreEqual(expected, Library.Day01.GetDistance(input));
+		}
+
+		[Test]
+		public void ProcessInput()
+		{
+			string input = "L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4";
+			int expected = 332;
+			Assert.AreEqual(expected, new Day01().ProcessInput(input));
+		}
+	}
+}
--- AdventOfCode.Library/Day01.cs
+++ AdventOfCode.Library/Day01.cs
@@ -5,8 +5,22 @@ namespace AdventOfCode.Library
 {
 	public class Day01
 	{
-		public Day01()
+		public int ProcessInput(string input)
 		{
+			Tuple<int, int> location = new Tuple<int, int>(0, 0);
+			Heading heading = Heading.North;
+			foreach (var navigationCommand in input.Split(','))
+			{
+				heading = GetNewHeading(heading, GetDirection(navigationCommand.Trim()[0]));
+				location = Move(location, heading, GetDistance(navigationCommand));
+			}
+			return Math.Abs(location.Item1) + Math.Abs(location.Item2);
+		}
+
+		public static Direction GetDirection(char v)
+		{
+			if (v == 'L') return Direction.Left;
+			return Direction.Right;
 		}
 
 		public static Heading GetNewHeading(Heading heading, Direction direction) {
@@ -20,12 +34,38 @@ namespace AdventOfCode.Library
 			}
 		}
 
+		Tuple<int, int> Move(Tuple<int, int> location, Heading heading, int distance)
+		{
+			switch (heading)
+			{
+				case Heading.North:
+					return new Tuple<int, int>(location.Item1, location.Item2 + distance);
+				case Heading.East:
+					return new Tuple<int, int>(location.Item1 + distance, location.Item2);
+				case Heading.South:
+					return new Tuple<int, int>(location.Item1, location.Item2 - distance);
+				case Heading.West:
+					return new Tuple<int, int>(location.Item1 - distance, location.Item2);
+				default:
+					return location;
+			}
+		}
+
+		public static int GetDistance(string navigationCommand)
+		{
+			return int.Parse(navigationCommand.Trim().Substring(1));
+		}
+
 		static Heading TurnLeft(Heading heading)
 		{
 			switch (heading)
 			{
 				case Heading.North:
 					return Heading.West;
+				case Heading.South:
+					return Heading.East;
+				case Heading.West:
+					return Heading.South;
 				default:
 					return Heading.North;
 			}
@@ -37,6 +77,10 @@ namespace AdventOfCode.Library
 			{
 				case Heading.North:
 					return Heading.East;
+				case Heading.East:
+					return Heading.South;
+				case Heading.South:
+					return Heading.West;
 				default:
 					return Heading.North;
 			}
